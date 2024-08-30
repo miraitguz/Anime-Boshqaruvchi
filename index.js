c78a70a33839288a2ed4743ec0eb5f6efc8f01fe
miraitguz <127748384+miraitguz@users.noreply.github.com>
2024-08-30 19:18:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 // Replace with your own token from @BotFather
-const token = '7227447533:AAGgFuiJfGFaz92tH4VJCEO4SmXeX-6e4C4';
+const token = process.env.BOT;
 const bot = new TelegramBot(token, { polling: true });
 
 // Store user data
